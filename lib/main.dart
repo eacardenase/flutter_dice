d4d603f12952929e36756f7f2f066a891385c963
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-30 07:14:46 -0500

    feat: extract ui logic into its own widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Colors.cyan.shade300,
-            Colors.pink.shade300,
-          ],
-        )),
-        child: const Center(
-          child: Text(
-            "This is just a test",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
+      body: GradientContainer(),
+    ),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Colors.cyan.shade300,
+          Colors.pink.shade300,
+        ],
+      )),
+      child: const Center(
+        child: Text(
+          "This is just a test",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
