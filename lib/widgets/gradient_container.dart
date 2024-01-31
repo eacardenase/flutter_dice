951cc86b301441c0bb55c69deb6bf56b8c12d6ea
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-30 19:04:45 -0500

    refactor: make gradient container widget reusable

--- lib/widgets/gradient_container.dart
+++ lib/widgets/gradient_container.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:dice_app/widgets/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({
+    super.key,
+    this.colors = const [Colors.cyan, Colors.pink],
+  });
+
+  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +17,7 @@ class GradientContainer extends StatelessWidget {
           gradient: LinearGradient(
         begin: Alignment.topLeft,
         end: Alignment.bottomRight,
-        colors: [
-          Colors.cyan.shade300,
-          Colors.pink.shade300,
-        ],
+        colors: colors,
       )),
       child: const Center(
         child: StyledText("This is just a test"),
